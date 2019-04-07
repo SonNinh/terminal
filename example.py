a3d16b99e2038035c685179c69082e8280724a9b
SonNinh <SonNinh@Sons-MacBook-Air.local>
2019-04-07 21:17:32 +0700

    "2nd"

--- example.py
+++ example.py
@@ -8,60 +8,27 @@ class Screen(object):
     DOWN = 1
 
     def __init__(self, items):
-        """ Initialize the screen window
-
-        Attributes
-            window: A full curses screen window
-
-            width: The width of `window`
-            height: The height of `window`
-
-            max_lines: Maximum visible line count for `result_window`
-            top: Available top line position for current page (used on scrolling)
-            bottom: Available bottom line position for whole pages (as length of items)
-            current: Current highlighted line number (as window cursor)
-            page: Total page count which being changed corresponding to result of a query (starts from 0)
-
-            ┌--------------------------------------┐
-            |1. Item                               |
-            |--------------------------------------| <- top = 1
-            |2. Item                               |
-            |3. Item                               |
-            |4./Item///////////////////////////////| <- current = 3
-            |5. Item                               |
-            |6. Item                               |
-            |7. Item                               |
-            |8. Item                               | <- max_lines = 7
-            |--------------------------------------|
-            |9. Item                               |
-            |10. Item                              | <- bottom = 10
-            |                                      |
-            |                                      | <- page = 1 (0 and 1)
-            └--------------------------------------┘
-
-        Returns
-            None
-        """
         self.window = None
 
         self.width = 0
         self.height = 0
 
         self.init_curses()
-
-        self.items = items
-
-        self.max_lines = curses.LINES
-        self.top = 0
-        self.bottom = len(self.items)
-        self.current = 0
-        self.page = self.bottom // self.max_lines
          
-        # self.text = subprocess.run('ls', stdout=subprocess.PIPE).stdout.decode()
-        f = open('text', 'r')
-        self.text = f.read()
-        f.close()
+        # self.text = subprocess.run('python3', stdout=subprocess.PIPE).stdout.decode()
+        # f = open('text', 'r')
+        # self.text = f.read()
+        # f.close()
+        self.prompt_name = 'intek-sh$ '
+        self.text = 'intek-sh$ '
+        self.lk=65
+        self.touch_ending = True
         self.last_char = -1
+        self.up_last = -1
+        self.down_last = -1
+        self.command = ''
+        self.lim_of_arrow = 0
+        self.pos_cursor_str = 0
         self.run()
 
     def init_curses(self):
@@ -90,83 +57,155 @@ class Screen(object):
             curses.endwin()
 
     def input_stream(self):
-        # result = run("ls", stdout=subprocess.PIPE)
-
+        self.window.clear()
+        self.window.refresh()
+        self.display()
+        # self.window.addstr(0, 0, self.text[-self.height*self.width-1:], curses.color_pair(1))
         """Waiting an input and run a proper method according to type of input"""
+
         while True:
-            self.display()
+            # self.display()
+            new_key = self.window.getch()
 
-            ch = self.window.getch()
-            if ch == curses.KEY_UP:
-                self.scroll(self.UP)
-            elif ch == curses.KEY_DOWN:
-                self.scroll(self.DOWN)
-            elif ch == curses.ascii.ESC:
+            self.lk = new_key
+            if new_key == curses.KEY_UP:
+                if self.touch_ending:
+                    self.last_char = self.up_last      
+                    self.display()       
+            elif new_key == curses.KEY_DOWN:
+                self.last_char = self.down_last
+                self.display()
+            elif new_key == curses.ascii.ESC:
                 break
-
-    def scroll(self, direction):
-        """Scrolling the window when pressing up/down arrow keys"""
-        # next cursor position after scrolling
-        next_line = self.current + direction
-
-        # Up direction scroll overflow
-        # current cursor position is 0, but top position is greater than 0
-        if (direction == self.UP) and (self.top > 0 and self.current == 0):
-            self.top += direction
-            return
-        # Down direction scroll overflow
-        # next cursor position touch the max lines, but absolute position of max lines could not touch the bottom
-        if (direction == self.DOWN) and (next_line == self.max_lines) and (self.top + self.max_lines < self.bottom):
-            self.top += direction
-            return
-        # Scroll up
-        # current cursor position or top position is greater than 0
-        if (direction == self.UP) and (self.top > 0 or self.current > 0):
-            self.current = next_line
-            return
-        # Scroll down
-        # next cursor position is above max lines, and absolute position of next cursor could not touch the bottom
-        if (direction == self.DOWN) and (next_line < self.max_lines) and (self.top + next_line < self.bottom):
-            self.current = next_line
-            return
+            elif new_key == 10:
+                self.display()
+                self.last_char = -1
+                self.up_last = -1
+                self.down_last = -1
+                self.play_subprocess()
+                self.display()
+            elif new_key < 127 and new_key > 31:
+                self.last_char = -1
+                self.up_last = -1
+                self.down_last = -1
+                self.display_key(new_key)
+                self.display()
+            elif new_key == 260 and self.pos_cursor_str > self.lim_of_arrow:
+                self.pos_cursor_str -= 1
+                self.move_cursor_back()
+            elif new_key == 261 and self.pos_cursor_str < 0:
+                self.pos_cursor_str += 1
+                self.move_cursor_forward()
+            elif new_key == 127:
+                self.delete_char()
+                self.display()
+            else:
+                self.display()
 
     def display(self):
         """Display the items on window"""
-        self.height = self.window.getmaxyx()[0]
-        self.width = self.window.getmaxyx()[1]
-        self.window.clear()
-        self.window.refresh()
-        lim_top, lim_bottom = self.get_top_bottom()
+        # self.window.clear()
+        # self.window.refresh()
+        self.height, self.width = self.window.getmaxyx()
 
-        self.window.addstr(0, 0, self.text[lim_top:lim_bottom], curses.color_pair(1))
+        # self.window.move(0, 0)
+        # self.window.deleteln()
+        self.window.move(self.height-1, 0)
+        self.window.deleteln()
+        self.get_top_bottom()
+
+        if self.last_char+1 >= 0: 
+            self.window.addstr(0, 0, self.text[self.last_char-self.height*self.width:], curses.color_pair(1))
+        else:
+            self.window.addstr(0, 0, self.text[self.last_char-self.height*self.width:self.last_char+1], curses.color_pair(1))
         self.window.refresh()
-        
-        # txt = self.text[lim_top:lim_top+5]
-        # self.window.addstr(self.window.getmaxyx()[0]-1, 0, txt, curses.color_pair(1))
+        self.window.clrtobot()
+        if curses.getsyx()[0] == self.height-1:
+            self.touch_ending = True
+        else:
+            self.touch_ending = False
+
+        if self.text[self.last_char] == '\n':
+            self.up_last -= 1
+        else:
+            self.up_last = self.last_char - 1
+            if self.text[self.up_last] != '\n':
+                self.up_last = self.last_char - curses.getsyx()[1]
+                try:
+                    if self.up_last == self.last_char:
+                        self.up_last -= self.width
+                    if self.text[self.up_last] == '\n':
+                        self.up_last -= 1
+                except IndexError:
+                    self.up_last = self.last_char
+
+        for _ in range(-self.pos_cursor_str):
+            self.move_cursor_back()
+            self.window.refresh()
+        # txt = str(self.last_char) + ' ' + str(self.up_last) + ' ' + str(self.down_last) + ' ' + str(curses.getsyx()[1])
+        # self.window.addstr(0, self.width-len(txt)-1, txt, curses.color_pair(2))
         # self.window.refresh()
 
     def get_top_bottom(self):
-        filled_line = 0
-        i = self.last_char
-        j = self.last_char
-        while i > -len(self.text):
-            if self.text[i] == '\n':
-                filled_line += (j-i)//self.width + 1
-                j = i
-                if filled_line > self.height:
-                    diff = filled_line - self.height
-                    i += diff*self.width
+        i = self.last_char + 1
+        if self.text[i] == '\n':
+            i += 1
+        for n in range(self.width):
+            if i+n < 0:
+                self.down_last = i+n
+                if self.text[i+n] == '\n':
+                    self.down_last = i+n-1
                     break
-                
-            i -= 1
-        
-        return i, self.last_char
 
+    def move_cursor_back(self):
+        y_cursor, x_cursor = curses.getsyx()
+        if x_cursor == 0:
+            if y_cursor != 0:
+                self.window.move(y_cursor-1, self.width-1)
+        else:
+            self.window.move(y_cursor, x_cursor-1)
 
+    def move_cursor_forward(self):
+        y_cursor, x_cursor = curses.getsyx()
+        if x_cursor == self.width-1:
+            self.window.move(y_cursor+1, 0)
+        else:
+            self.window.move(y_cursor, x_cursor+1)
 
+    def display_key(self, new_key):
+        self.lim_of_arrow -= 1
+        if self.pos_cursor_str == 0:
+            self.text += chr(new_key)
+            self.command += chr(new_key)
+        else:
+            self.text = self.text[:self.pos_cursor_str] + chr(new_key) + self.text[self.pos_cursor_str:]
+            self.command = self.command[:self.pos_cursor_str] + chr(new_key) + self.command[self.pos_cursor_str:]
+    
+            
+    def play_subprocess(self):
+        self.lim_of_arrow = 0
+        self.pos_cursor_str = 0
+        self.text += '\n'
+        command_for_sub = self.command.split()
+        self.command = ''
+        if command_for_sub:
+            self.text += subprocess.run(command_for_sub, stdout=subprocess.PIPE).stdout.decode()
+            if self.text[-1] != '\n':
+                self.text += '\n'
+        self.text += self.prompt_name
+
+    def delete_char(self):
+        if self.pos_cursor_str > self.lim_of_arrow:
+            self.lim_of_arrow += 1
+            if self.pos_cursor_str != 0:
+                self.text = self.text[:self.pos_cursor_str-1] + self.text[self.pos_cursor_str:]
+                self.command = self.command[:self.pos_cursor_str-1] + self.command[self.pos_cursor_str:]
+            else:
+                self.text = self.text[:self.pos_cursor_str-1]
+                self.command = self.command[:self.pos_cursor_str-1]
 
 def main():
-    items = ['{}. Itemmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmsdfgndfrtdfvbar'.format(num) for num in range(10)]
+    items = ['{}. Itemfgndfrtdfvbar'.format(num) for num in range(10)]
     Screen(items)
 
 

ffc10c7133f387d6c17eb6b433892d4b42248277
SonNinh <sonninhngocba@gmail.com>
2020-08-24 10:03:42 +0700

    Delete fuck.py

--- fuck.py
+++
@@ -1,8 +0,0 @@
-import sys
-from subprocess import Popen, PIPE
-
-with Popen(['python3'], stdin=PIPE, stdout=PIPE,
-           universal_newlines=True, bufsize=1) as cat:
-    for input_string in ["print(\'sdfsndf\')", "print(12)", ""]:
-        print(input_string, file=cat.stdin, flush=True)
-        print(cat.stdout.readline(), end='')

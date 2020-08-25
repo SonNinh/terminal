61ac6605235f22248733870a0b7cb253fb153359
SonNinh <sonninhngocba@gmail.com>
2020-08-25 22:28:10 +0700

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-f = open('text', 'r')
-text = f.read()
-f.close()
-
-while True:
-    args = input("input: ")
-    ls = args.split()
-
-    print(text[-int(ls[0])-3:-int(ls[0])+1] + ':' + text[-int(ls[1])-3:-int(ls[1])+1])
